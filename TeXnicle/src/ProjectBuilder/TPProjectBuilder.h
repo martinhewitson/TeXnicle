01b4d2422a7fdcfd6bd91b50ffebf036ba499ac2
Martin Hewitson <hewitson@hewitsonair.home>
2011-07-30 10:22:03 +0200

    Start of a new project builder class.

---
+++ TeXnicle/src/ProjectBuilder/TPProjectBuilder.h
@@ -0,0 +1,13 @@
+//
+//  TPProjectBuilder.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 30/7/11.
+//  Copyright 2011 bobsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPProjectBuilder : NSObject
+
+@end
---
+++ TeXnicle/src/ProjectBuilder/TPProjectBuilder.m
@@ -0,0 +1,23 @@
+//
+//  TPProjectBuilder.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 30/7/11.
+//  Copyright 2011 bobsoft. All rights reserved.
+//
+
+#import "TPProjectBuilder.h"
+
+@implementation TPProjectBuilder
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
