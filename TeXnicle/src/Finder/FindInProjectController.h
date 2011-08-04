107993e0a88d74c2ce17e543d99169aa76880943
Martin Hewitson <hewitson@HewitsonAir.local>
2011-08-04 15:19:49 +0200

    Implemented project searching with GCD and act like outline view delegate and datasource.

--- TeXnicle/src/Finder/FindInProjectController.h
+++ TeXnicle/src/Finder/FindInProjectController.h
@@ -11,30 +11,52 @@
 @class TeXProjectDocument;
 @class ProjectEntity;
 @class FileEntity;
+@class TPResultDocument;
+@class FindInProjectController;
 
 @protocol FindInProjectControllerDelegate <NSObject>
 
 - (ProjectEntity*)project;
 - (void) highlightSearchResult:(NSString*)result withRange:(NSRange)aRange inFile:(FileEntity*)aFile;
 
+- (void)didBeginSearch:(FindInProjectController*)aFinder;
+- (void)didEndSearch:(FindInProjectController*)aFinder;
+- (void)didMakeMatch:(FindInProjectController*)aFinder;
+- (void)didCancelSearch:(FindInProjectController*)aFinder;
+
 @end
 
-@interface FindInProjectController : NSWindowController {
+@interface FindInProjectController : NSWindowController <NSOutlineViewDelegate, NSOutlineViewDataSource> {
 
-	IBOutlet NSTableView *resultsView;
-//	IBOutlet NSTextField *searchText;
-//	IBOutlet NSButton *findButton;
-	
-	IBOutlet NSArrayController *searchResults;
-	
-  id<FindInProjectControllerDelegate> delegate;
+  IBOutlet NSOutlineView *resultsOutlineView;	
+  id<FindInProjectControllerDelegate> delegate;  
+  NSMutableArray *results;
+  NSCharacterSet *ws;
+  NSCharacterSet *ns;
+  
+	dispatch_queue_t queue;
+  dispatch_semaphore_t arrayLock;
+  
+  NSInteger filesProcessed;
+  BOOL isSearching;
+  BOOL shouldContinueSearching;
+  BOOL shouldSearchOnEnd;
+  
 }
+
 @property (assign) IBOutlet id<FindInProjectControllerDelegate> delegate;
+@property (retain) NSMutableArray *results;
+
 
 - (IBAction) performSearch:(id)sender;
-- (IBAction)handleTableDoubleClick:(id)sender;
 - (void)searchForTerm:(NSString*)searchTerm;
+- (void) searchForTerm:(NSString *)searchTerm inFile:(FileEntity*)file;
+- (IBAction)handleOutlineViewDoubleClick:(id)sender;
+- (void) jumpToResultAtRow:(NSInteger)aRow;
 - (void) jumpToSearchResult:(NSInteger)index;
 - (NSInteger)count;
+- (id)resultAtIndex:(NSInteger)anIndex;
+
+- (TPResultDocument*)resultDocumentForDocument:(FileEntity*)aFile;
 
 @end
--- TeXnicle/src/Finder/FindInProjectController.m
+++ TeXnicle/src/Finder/FindInProjectController.m
@@ -15,25 +15,49 @@
 #import "NSString+LaTeX.h"
 #import "NSMutableAttributedString+CodeFolding.h"
 #import "RegexKitLite.h"
+#import "TPResultDocument.h"
+#import "TPDocumentMatch.h"
 
 @implementation FindInProjectController
 
 @synthesize delegate;
+@synthesize results;
 
 - (id) init
 {
-	
-	if (![super initWithWindowNibName:@"FindInProject"])
-		return nil;
-	
+	self = [super initWithWindowNibName:@"FindInProject"];
+  
+  if (self) {
+    
+    self.results = [NSMutableArray array];
+    ws = [[NSCharacterSet whitespaceCharacterSet] retain];
+    ns = [[NSCharacterSet newlineCharacterSet] retain];
+    
+    queue = dispatch_queue_create("com.bobsoft.TeXnicle", NULL);
+    dispatch_queue_t priority = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);    
+    dispatch_set_target_queue(queue,priority);
+    
+    arrayLock = dispatch_semaphore_create(1);
+
+  }
 	
 	return self;
 }
 
-- (void) awakeFromNib
+- (void) dealloc
 {
-	[resultsView setDoubleAction:@selector(handleTableDoubleClick:)];
-  [resultsView setTarget:self];
+  [ws release];
+  [ns release];
+	dispatch_release(queue);
+  dispatch_release(arrayLock);
+  self.results = nil;
+  [super dealloc];
+}
+
+- (void) awakeFromNib
+{  
+  [resultsOutlineView setDoubleAction:@selector(handleOutlineViewDoubleClick:)];
+  [resultsOutlineView setTarget:self];
 }
 
 
@@ -55,10 +79,64 @@
 
 - (void)searchForTerm:(NSString*)searchTerm
 {
+  shouldSearchOnEnd = NO;
+  if (isSearching) {
+    shouldContinueSearching = NO;
+    shouldSearchOnEnd = YES;
+    return;
+  }
+  
   if ([searchTerm length] == 0) {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(didEndSearch:)]) {
+      [self.delegate didEndSearch:self];
+    }
     return;
   }
 	
+  if (self.delegate && [self.delegate respondsToSelector:@selector(didBeginSearch:)]) {
+    [self.delegate didBeginSearch:self];
+  }
+	
+  //	NSString *regexp = [NSString stringWithFormat:@".*%@.*", searchTerm];
+  //	NSString *regexp = [NSString stringWithFormat:@"(\\n)?.*%@.*(\\n)?", searchTerm];
+	
+  ProjectEntity *project = [self.delegate project];
+	
+//  NSLog(@"Searching for '%@' in project %@", searchTerm, [project valueForKey:@"name"]);
+//  NSLog(@"Searching with regexp: %@", regexp);
+	
+  dispatch_semaphore_wait(arrayLock, DISPATCH_TIME_FOREVER);
+  [self.results removeAllObjects];  
+  shouldContinueSearching = YES;
+  isSearching = YES;
+  dispatch_semaphore_signal(arrayLock);
+  
+  
+	// go through each doc in the project
+  NSArray *items = [project valueForKey:@"items"];
+  filesProcessed = 0;
+	for (ProjectItemEntity *item in items) {
+		if ([item isKindOfClass:[FileEntity class]]) {
+			
+			FileEntity *file = (FileEntity*)item;
+			if ([[file valueForKey:@"isText"] boolValue]) {
+        dispatch_async(queue, ^{						
+          NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];          
+          [self searchForTerm:searchTerm inFile:file];
+          [pool drain];      
+        });
+        filesProcessed++;
+			} // end if isText			
+		} // end if is file
+	} // end loop over project items
+  
+}
+
+- (void) searchForTerm:(NSString *)searchTerm inFile:(FileEntity*)file
+{
+  // get the text for this file
+  FileDocument *doc = [file document];
+  
 	NSArray *searchTerms = [searchTerm componentsSeparatedByString:@" "];
   if ([searchTerms count] == 0) {
     return;
@@ -69,104 +147,247 @@
 		[regexp appendFormat:@"%@(\\s)*(\\n)?", term];
 	}
 	[regexp appendFormat:@".*(\\n)?"];
-	
-  //	NSString *regexp = [NSString stringWithFormat:@".*%@.*", searchTerm];
-  //	NSString *regexp = [NSString stringWithFormat:@"(\\n)?.*%@.*(\\n)?", searchTerm];
-	
-  ProjectEntity *project = [self.delegate project];
-	
-//  NSLog(@"Searching for '%@' in project %@", searchTerm, [project valueForKey:@"name"]);
-//  NSLog(@"Searching with regexp: %@", regexp);
-	
-	[searchResults removeObjects:[searchResults arrangedObjects]];
   
-	// go through each doc in the project
-	for (ProjectItemEntity *item in [project valueForKey:@"items"]) {
-		if ([item isKindOfClass:[FileEntity class]]) {
-			
-			FileEntity *file = (FileEntity*)item;
-			if ([[file valueForKey:@"isText"] boolValue]) {
-				
-        //				NSLog(@"Searching %@...", [file valueForKey:@"name"]);
-				
-				// get the text for this file
-				FileDocument *doc = [file document];
+  NSMutableAttributedString *aStr = [[doc textStorage] mutableCopy];
+  NSString *string = [aStr unfoldedString];
+  [aStr release];
+  if (!string)
+    return;
+  
+  NSArray *regexpresults = [string componentsMatchedByRegex:regexp];
+  
+  NSScanner *aScanner = [NSScanner scannerWithString:string];
+  BOOL didMatch = NO;
+  if ([regexpresults count] > 0) {
+    
+    for (NSString *result in regexpresults) {
+      if (!shouldContinueSearching) {
+        break;
+      } // If should continue 
+      
+      NSString *returnResult = [NSString stringWithControlsFilteredForString:result];
+      
+      returnResult = [result stringByTrimmingCharactersInSet:ws];
+      returnResult = [returnResult stringByTrimmingCharactersInSet:ns];
+      
+      if ([aScanner scanUpToString:returnResult intoString:NULL]) {
         
-				NSMutableAttributedString *aStr = [[doc textStorage] mutableCopy];
-				NSString *string = [aStr unfoldedString];
-				[aStr release];
-        //				NSString *string = [[doc textStorage] string];
-				if (!string)
-					return;
-				
-        //				NSLog(@"Searching string %@", string);
-				
-				
-				
-				NSArray *results = [string componentsMatchedByRegex:regexp];
-				
-				NSScanner *aScanner = [NSScanner scannerWithString:string];
-        //				NSLog(@"Results: %@", results);
-				if ([results count] > 0) {
-					
-					for (NSString *result in results) {
+        NSRange resultRange = NSMakeRange([aScanner scanLocation], [returnResult length]);
+        if (resultRange.location != NSNotFound) {
+          
+          NSRange subrange    = [returnResult rangeOfRegex:[searchTerms objectAtIndex:0]];
+          if (subrange.location != NSNotFound) {
+            resultRange.location += subrange.location;
+            resultRange.length = [searchTerm length];
             
-						NSString *returnResult = [NSString stringWithControlsFilteredForString:result];
-						
-            //						returnResult = [result stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-						returnResult = [result stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-						
-						if ([aScanner scanUpToString:returnResult intoString:NULL]) {
-							
-							NSRange resultRange = NSMakeRange([aScanner scanLocation], [returnResult length]);
-							
-							NSRange subrange    = [returnResult rangeOfRegex:[searchTerms objectAtIndex:0]];
-							resultRange.location += subrange.location;
-							resultRange.length = [searchTerm length];
-              //							NSLog(@"Got results range:%@", NSStringFromRange(resultRange));
-							
-							NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-							[dict setObject:file forKey:@"document"];
-							[dict setObject:NSStringFromRange(resultRange) forKey:@"range"];
-							[dict setObject:[NSString stringWithControlsFilteredForString:returnResult] forKey:@"result"];
-							[searchResults addObject:dict];
-              //							NSLog(@"Found %@", returnResult);
-							
-							[resultsView reloadData];
-						}
-					} // end loop over results
-				} // end if [results count] > 0
-			} // end if isText			
-		} // end if is file
-	} // end loop over project items
+            TPResultDocument *resultDoc = [self resultDocumentForDocument:file];
+            
+            // scan back to start of word
+            NSInteger idx = subrange.location;
+            while (idx > 0) {
+              if ([ws characterIsMember:[returnResult characterAtIndex:idx]]) {
+                break;
+              }
+              idx--;
+            }
+            NSInteger len = MIN(subrange.location-idx+30, [returnResult length]-idx);
+            len = MAX(len, [searchTerm length]);
+            NSString *matchingString = [returnResult substringWithRange:NSMakeRange(idx, len)];
+            if (idx>0) {
+              matchingString = [@"..." stringByAppendingString:matchingString];
+              idx-=3;
+            }
+            
+            TPDocumentMatch *match = [TPDocumentMatch documentMatchWithRange:resultRange subrange:NSMakeRange(subrange.location-idx, [searchTerm length]) matchingString:matchingString];
+            dispatch_semaphore_wait(arrayLock, DISPATCH_TIME_FOREVER);
+            [resultDoc addMatch:match];
+            if (![self.results containsObject:resultDoc]) {
+              [self.results addObject:resultDoc];
+            }
+            didMatch = YES;
+            dispatch_semaphore_signal(arrayLock);
+            
+            dispatch_sync(dispatch_get_main_queue(),
+                          // block
+                          ^{
+                            if (self.delegate && [self.delegate respondsToSelector:@selector(didMakeMatch:)]) {
+                              [self.delegate didMakeMatch:self];
+                            }
+                            [resultsOutlineView reloadData];
+                          });
+          } // end subrange found
+        } // end result range founds
+      } // end scanner
+    } // end loop over results
+  } // end if [results count] > 0
+  
+  filesProcessed--;
+  
+  // check if this is the last one?
+  if (filesProcessed == 0) {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(didEndSearch:)]) {
+      [self.delegate didEndSearch:self];
+    }
+    if (!shouldContinueSearching) {
+      // send cancelled message
+      if (self.delegate && [self.delegate respondsToSelector:@selector(didCancelSearch:)]) {
+        [self.delegate didCancelSearch:self];
+      }
+    }
+    isSearching = NO;
+    shouldContinueSearching = NO;
+    [resultsOutlineView reloadData];
+  }
+  
 }
 
-- (IBAction)handleTableDoubleClick:(id)sender
+- (TPResultDocument*)resultDocumentForDocument:(FileEntity*)aFile
 {
-	NSInteger row = [resultsView clickedRow];
+  for (TPResultDocument *doc in self.results) {
+    if (doc.document == aFile) {
+      return doc;
+    }
+  }
+  
+  return [TPResultDocument resultWithDocuemnt:aFile];
+}
+
+
+
+
+- (IBAction)handleOutlineViewDoubleClick:(id)sender
+{
+	NSInteger row = [resultsOutlineView clickedRow];
 	if (row < 0) {
 		// get selection
-		row = [resultsView selectedRow];
+		row = [resultsOutlineView selectedRow];
 	}
 	
 	if (row >= 0) {
-		
-		// get file
-		[self jumpToSearchResult:row];
+    id item = [resultsOutlineView itemAtRow:row];
+		if ([item isKindOfClass:[TPDocumentMatch class]]) {
+      [self jumpToResultAtRow:row];
+    } else if ([item isKindOfClass:[TPResultDocument class]]) {
+      [[resultsOutlineView animator] expandItem:item];
+    }
 	}
 }
 
+
 - (NSInteger)count
 {
-  return [[searchResults arrangedObjects] count];  
+  NSInteger count = 0;
+  for (TPResultDocument *doc in self.results) {
+    for (TPDocumentMatch *match in doc.matches) {
+      count++;
+    }
+  }
+  return count;  
 }
+
+- (void) jumpToResultAtRow:(NSInteger)aRow
+{
+  id item = [resultsOutlineView itemAtRow:aRow];
+//  NSLog(@"Found item %@ at index %ld: %@", [item class], aRow, item);
+  if ([item isKindOfClass:[TPDocumentMatch class]]) {
+    TPDocumentMatch *match = (TPDocumentMatch*)item;
+    TPResultDocument *doc = [resultsOutlineView parentForItem:item];
+    
+    [self.delegate highlightSearchResult:match.match 
+                               withRange:match.range
+                                  inFile:[doc valueForKey:@"document"]];
+    
+  }
+}
+
 
 - (void) jumpToSearchResult:(NSInteger)index
 {
-  FileEntity *file = [[searchResults arrangedObjects] objectAtIndex:index];
-  [self.delegate highlightSearchResult:[file valueForKey:@"result"] 
-                             withRange:NSRangeFromString([file valueForKey:@"range"])
-                                inFile:[file valueForKey:@"document"]];
+  id item = [self resultAtIndex:index];
+//  NSLog(@"Found item %@ at index %ld: %@", [item class], index, item);
+  if ([item isKindOfClass:[TPDocumentMatch class]]) {
+    TPDocumentMatch *match = (TPDocumentMatch*)item;
+    TPResultDocument *doc = [resultsOutlineView parentForItem:item];
+    
+    [self.delegate highlightSearchResult:match.match 
+                               withRange:match.range
+                                  inFile:[doc valueForKey:@"document"]];
+    
+  }
 }
+
+- (id)resultAtIndex:(NSInteger)anIndex
+{
+  NSInteger count = 0;
+  for (TPResultDocument *doc in self.results) {
+    for (TPDocumentMatch *match in doc.matches) {
+      if (count == anIndex) {
+        return match;
+      }
+      count++;
+    }
+  }
+  return nil;
+}
+
+#pragma mark -
+#pragma mark SearchResults OutlineView delegate
+
+- (BOOL) outlineView:(NSOutlineView *)outlineView shouldSelectItem:(id)item
+{
+  if ([item isKindOfClass:[TPDocumentMatch class]]) {
+    return YES;
+  }
+  return NO;
+}
+
+#pragma mark -
+#pragma mark SearchResults OutlineView datasource
+
+- (BOOL) outlineView:(NSOutlineView *)outlineView isGroupItem:(id)item
+{
+  if ([item isKindOfClass:[TPResultDocument class]]) {
+    return YES;
+  }
+  
+  return NO;
+}
+
+- (id) outlineView:(NSOutlineView *)outlineView objectValueForTableColumn:(NSTableColumn *)tableColumn byItem:(id)item
+{
+  return [item valueForKey:@"displayString"];
+}
+
+- (BOOL) outlineView:(NSOutlineView *)outlineView isItemExpandable:(id)item
+{
+  if ([item isKindOfClass:[TPResultDocument class]]) {
+    return [[item valueForKey:@"matches"] count] > 0;
+  }
+  
+  return NO;
+}
+
+- (id) outlineView:(NSOutlineView *)outlineView child:(NSInteger)index ofItem:(id)item
+{
+  if (item == nil) {
+    return [self.results objectAtIndex:index];
+  }
+  
+  return [[item valueForKey:@"matches"] objectAtIndex:index];  
+}
+
+- (NSInteger)outlineView:(NSOutlineView *)outlineView numberOfChildrenOfItem:(id)item
+{
+  if (item == nil) {
+    return [self.results count];
+  }
+  
+  if ([item isKindOfClass:[TPResultDocument class]]) {
+    return [[item valueForKey:@"matches"] count];
+  }
+  
+  return 0;
+}
+
 
 @end
