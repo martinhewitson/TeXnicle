1492c87be8b42e890014ab8efe2f40eeb41d4b0f
Martin Hewitson <hewitson@localhost.(none)>
2011-08-28 07:14:12 +0200

    stroked filled background views.

---
+++ TeXnicle/Engine/MHStrokedFiledView.h
@@ -0,0 +1,16 @@
+//
+//  MHStrokedFiledView.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 28/08/11.
+//  Copyright 2011 bobsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MHStrokedFiledView : NSView
+
+@property (retain) NSColor *fillColor;
+@property (retain) NSColor *strokeColor;
+
+@end
---
+++ TeXnicle/Engine/MHStrokedFiledView.m
@@ -0,0 +1,49 @@
+//
+//  MHStrokedFiledView.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 28/08/11.
+//  Copyright 2011 bobsoft. All rights reserved.
+//
+
+#import "MHStrokedFiledView.h"
+
+@implementation MHStrokedFiledView
+
+@synthesize fillColor;
+@synthesize strokeColor;
+
+- (id)initWithFrame:(NSRect)frame
+{
+  self = [super initWithFrame:frame];
+  if (self) {
+    // Initialization code here.
+    self.fillColor = [NSColor controlBackgroundColor];
+    self.strokeColor = [NSColor darkGrayColor];
+  }
+  
+  return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+  // Drawing code here.
+  NSRect b = [self bounds];
+  
+  NSRect r = NSInsetRect(b, 0.5, 0.5);
+
+  NSBezierPath *path = [NSBezierPath bezierPath];
+  [path moveToPoint:NSMakePoint(r.origin.x, r.origin.y)];
+  [path lineToPoint:NSMakePoint(r.origin.x+r.size.width, r.origin.y)];
+  [path moveToPoint:NSMakePoint(r.origin.x, r.origin.y+r.size.height)];
+  [path lineToPoint:NSMakePoint(r.origin.x+r.size.width, r.origin.y+r.size.height)];
+  
+  [path setLineWidth:1.0];
+  [self.strokeColor set];
+  [path stroke];
+  [self.fillColor set];
+  NSRectFill(r);
+  
+}
+
+@end
