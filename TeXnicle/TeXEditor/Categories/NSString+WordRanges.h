fd90f94716b318a8cf4e4faafb8f3e94aa6055b5
hewitson <martin.hewitson@aei.mpg.de>
2011-09-07 08:48:42 +0200

    New category to get the ranges of words in the text.

---
+++ TeXnicle/TeXEditor/Categories/NSString+WordRanges.h
@@ -0,0 +1,15 @@
+//
+//  NSString+WordRanges.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 5/9/11.
+//  Copyright 2011 bobsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (WordRanges)
+
+- (NSArray*) rangesOfString: (NSString *)subString ;
+
+@end
---
+++ TeXnicle/TeXEditor/Categories/NSString+WordRanges.m
@@ -0,0 +1,34 @@
+//
+//  NSString+WordRanges.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 5/9/11.
+//  Copyright 2011 bobsoft. All rights reserved.
+//
+
+#import "NSString+WordRanges.h"
+
+@implementation NSString (WordRanges)
+
+- (NSArray*) rangesOfString: (NSString *)subString 
+{
+  NSMutableArray *matches = [NSMutableArray array];
+  NSUInteger myLength = [self length];
+  NSRange uncheckedRange = NSMakeRange(0, myLength);
+  for(;;) {
+    NSRange foundAtRange = [self rangeOfString:subString
+                                       options:0
+                                         range:uncheckedRange];
+    if (foundAtRange.location != NSNotFound) {
+      
+      [matches addObject:[NSValue valueWithRange:foundAtRange]];
+      NSUInteger newLocation = NSMaxRange(foundAtRange); 
+      uncheckedRange = NSMakeRange(newLocation, myLength-newLocation);
+    } else {
+      break;
+    }
+  }
+  return matches;
+}
+
+@end
