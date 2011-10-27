ad28fb99c49a5fe1a0e1cdf4d1db6f524688a78e
Martin Hewitson <hewitson@HewitsonAir.local>
2011-10-27 06:25:48 +0100

    A new mini console which conforms to MHConsoleViewer protocol.

---
+++ MHMiniConsoleViewController.h
@@ -0,0 +1,19 @@
+//
+//  MHMiniConsoleViewController.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 26/10/11.
+//  Copyright (c) 2011 bobsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MHConsoleViewer.h"
+
+@interface MHMiniConsoleViewController : NSViewController <MHConsoleViewer> {
+@private
+  NSTextField *textField;
+}
+
+@property (assign) IBOutlet NSTextField *textField;
+
+@end
---
+++ MHMiniConsoleViewController.m
@@ -0,0 +1,59 @@
+//
+//  MHMiniConsoleViewController.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 26/10/11.
+//  Copyright (c) 2011 bobsoft. All rights reserved.
+//
+
+#import "MHMiniConsoleViewController.h"
+
+@implementation MHMiniConsoleViewController
+
+@synthesize textField;
+
+- (id)init
+{
+  self = [super initWithNibName:@"MHMiniConsoleViewController" bundle:nil];
+  if (self) {
+    // Initialization code here.
+  }
+  
+  return self;
+}
+
+- (void) clear
+{
+  [self.textField setStringValue:@""];
+  [self.textField setNeedsDisplay];
+  [self.textField setToolTip:@""];
+}
+
+- (void) appendText:(NSString*)someText
+{
+  // do nothing
+}
+
+- (void) appendText:(NSString*)someText withColor:(NSColor*)aColor
+{
+  // do nothing
+}
+
+- (void) error:(NSString*)someText
+{
+  [self.textField setTextColor:[NSColor redColor]];
+  [self.textField setStringValue:someText];
+  [self.textField setNeedsDisplay];
+  [self.textField setToolTip:someText];
+}
+
+- (void)message:(NSString *)someText
+{
+  [self.textField setTextColor:[NSColor darkGrayColor]];
+  [self.textField setStringValue:someText];
+  [self.textField setNeedsDisplay];
+  [self.textField setToolTip:someText];
+}
+
+
+@end
