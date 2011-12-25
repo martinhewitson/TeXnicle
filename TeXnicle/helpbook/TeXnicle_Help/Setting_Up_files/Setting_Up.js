24ec2bee3eacdae22d55511298d13d1cc8e005e9
Martin Hewitson <hewitson@localhost.(none)>
2011-12-25 17:09:36 +0100

    help update

--- TeXnicle/helpbook/TeXnicle_Help/Setting_Up_files/Setting_Up.js
+++ TeXnicle/helpbook/TeXnicle_Help/Setting_Up_files/Setting_Up.js
@@ -1,11 +1,11 @@
-// Created by iWeb 3.0.3 local-build-20110605
+// Created by iWeb 3.0.4 local-build-20111225
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Setting_Up_files/Setting_UpMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
