73e2e590d1ae819c8c4af234d76fc6566328cfaa
Martin Hewitson <hewitson@localhost.(none)>
2012-01-14 12:19:50 +0100

    Make protocol optional

--- TeXnicle/Engine/TPEngineManager.h
+++ TeXnicle/Engine/TPEngineManager.h
@@ -14,6 +14,7 @@ extern NSString * const TPEngineCompilingCompletedNotification;
 extern NSString * const TPEngineDidTrashFilesNotification;
 
 @protocol TPEngineManagerDelegate <NSObject>
+@optional
 -(NSString*)engineName;
 -(NSNumber*)doBibtex;
 -(NSNumber*)doPS2PDF;
