be639ba6cdd7f76da1e933a597ec260c1aa7edb6
Martin Hewitson <hewitson@localhost.(none)>
2012-02-02 20:02:15 +0100

    Tableview subclass that does nothing at the moment.

---
+++ TeXnicle/src/TemplateEditor/TPTemplateTableView.h
@@ -0,0 +1,13 @@
+//
+//  TPTemplateTableView.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 01/02/12.
+//  Copyright (c) 2012 bobsoft. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface TPTemplateTableView : NSTableView
+
+@end
---
+++ TeXnicle/src/TemplateEditor/TPTemplateTableView.m
@@ -0,0 +1,26 @@
+//
+//  TPTemplateTableView.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 01/02/12.
+//  Copyright (c) 2012 bobsoft. All rights reserved.
+//
+
+#import "TPTemplateTableView.h"
+
+@implementation TPTemplateTableView
+
+//- (BOOL)performKeyEquivalent:(NSEvent *)theEvent
+//{
+//  NSLog(@"Event %@", theEvent);
+//  
+//  return [super performKeyEquivalent:theEvent];
+//}
+//
+//- (void)keyDown:(NSEvent *)theEvent
+//{
+//  NSLog(@"Event %@", theEvent);
+//  [super keyDown:theEvent];
+//}
+
+@end
