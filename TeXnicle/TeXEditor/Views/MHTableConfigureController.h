aaf60f2dcb68e01149bd5f9e41d5922d88bad2e1
Martin Hewitson <hewitson@localhost.(none)>
2012-02-14 08:29:15 +0100

    Added ivars.

--- TeXnicle/TeXEditor/Views/MHTableConfigureController.h
+++ TeXnicle/TeXEditor/Views/MHTableConfigureController.h
@@ -19,6 +19,9 @@
 @private
   NSUInteger numberOfRows;
   NSUInteger numberOfColumns;
+  id<MHTableConfigureDelegate> delegate;
+  NSTextField *numRowsField;
+  NSTextField *numColsField;
 }
 
 @property (assign) id<MHTableConfigureDelegate> delegate;
