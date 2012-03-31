259fed5227f3d737775ce2ccd1dca63b9e1a6c7f
Martin Hewitson <martin@bobsoft-mac.de>
2012-03-31 15:00:58 +0200

    Update

--- TeXnicle/helpbook/TeXnicle_Help/Adding_Files_files/Adding_Files.js
+++ TeXnicle/helpbook/TeXnicle_Help/Adding_Files_files/Adding_Files.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.3 local-build-20110605
+// Created by iWeb 3.0.4 local-build-20111225
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(1.4142,1.4142),color:'#000000',opacity:0.500000})});registry.applyEffects();}
