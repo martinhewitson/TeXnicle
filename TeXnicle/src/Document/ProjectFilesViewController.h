bcec17dff3e124a0f2748be503f3bc296b9b4eca
Martin Hewitson <martin@bobsoft-mac.de>
2012-05-03 10:22:15 +0100

    Empty infrastructure to move the project files outline view to its own nib. It's quite a lot of work to do this, but will be better in the end.

---
+++ TeXnicle/src/Document/ProjectFilesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProjectFilesViewController.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 28/4/12.
+//  Copyright (c) 2012 bobsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ProjectFilesViewController : NSViewController
+
+@end
---
+++ TeXnicle/src/Document/ProjectFilesViewController.m
@@ -0,0 +1,27 @@
+//
+//  ProjectFilesViewController.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 28/4/12.
+//  Copyright (c) 2012 bobsoft. All rights reserved.
+//
+
+#import "ProjectFilesViewController.h"
+
+@interface ProjectFilesViewController ()
+
+@end
+
+@implementation ProjectFilesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
