c3cbbef4d39a62db27d8570b593031717db82445
Martin Hewitson <martin@bobsoft-mac.de>
2012-07-13 08:17:13 +0200

    Added license text. Refactored string display methods.

--- TeXnicle/src/SpellChecker/TPMisspelledWord.m
+++ TeXnicle/src/SpellChecker/TPMisspelledWord.m
@@ -5,6 +5,25 @@
 //  Created by Martin Hewitson on 07/07/12.
 //  Copyright (c) 2012 bobsoft. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL DAN WOOD, MIKE ABDULLAH OR KARELIA SOFTWARE BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 #import "TPMisspelledWord.h"
 
@@ -40,34 +59,16 @@
 
 - (NSAttributedString*)selectedDisplayString
 {
-  NSMutableParagraphStyle *ps = [[[NSMutableParagraphStyle alloc] init] autorelease];
-  [ps setParagraphStyle:[NSParagraphStyle defaultParagraphStyle]];
-  [ps setLineBreakMode:NSLineBreakByTruncatingTail];  
-  
-  NSString *text = self.word;
-  
-  NSMutableAttributedString *att = [[[NSMutableAttributedString alloc] initWithString:text] autorelease]; 
-  
-  [att addAttribute:NSForegroundColorAttributeName value:[NSColor redColor] range:NSMakeRange(0, [att length])];  
-  
-  if ([self.corrections count]>0) {
-    NSString *correctionString = [NSString stringWithFormat:@" ➜ [%@]", [self.corrections componentsJoinedByString:@", "]];
-    if ([correctionString length] > 100) {
-      correctionString = [NSString stringWithFormat:@"%@ ...", [correctionString substringToIndex:100]];
-    }
-    NSMutableAttributedString *str = [[[NSMutableAttributedString alloc] initWithString:correctionString] autorelease];
-    [str addAttribute:NSForegroundColorAttributeName value:[NSColor whiteColor] range:NSMakeRange(0, [str length])];  
-    [str addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]] range:NSMakeRange(0, [str length])];
-    [att appendAttributedString:str];
-  }  
-  
-  [att addAttribute:NSParagraphStyleAttributeName value:ps range:NSMakeRange(0, [att length])];
-  
-  return att;
+  return [self stringForDisplayWithColor:[NSColor alternateSelectedControlTextColor] detailsColor:[NSColor alternateSelectedControlTextColor]];
 }
 
 
 - (NSAttributedString*)displayString
+{
+  return [self stringForDisplayWithColor:[NSColor redColor] detailsColor:[NSColor darkGrayColor]];
+}
+
+- (NSAttributedString*)stringForDisplayWithColor:(NSColor*)color detailsColor:(NSColor*)detailsColor
 {
   NSMutableParagraphStyle *ps = [[[NSMutableParagraphStyle alloc] init] autorelease];
   [ps setParagraphStyle:[NSParagraphStyle defaultParagraphStyle]];
@@ -77,7 +78,7 @@
   
   NSMutableAttributedString *att = [[[NSMutableAttributedString alloc] initWithString:text] autorelease]; 
   
-  [att addAttribute:NSForegroundColorAttributeName value:[NSColor redColor] range:NSMakeRange(0, [att length])];  
+  [att addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(0, [att length])];  
   
   if ([self.corrections count]>0) {
     NSString *correctionString = [NSString stringWithFormat:@" ➜ [%@]", [self.corrections componentsJoinedByString:@", "]];
@@ -85,7 +86,7 @@
       correctionString = [NSString stringWithFormat:@"%@ ...", [correctionString substringToIndex:100]];
     }
     NSMutableAttributedString *str = [[[NSMutableAttributedString alloc] initWithString:correctionString] autorelease];
-    [str addAttribute:NSForegroundColorAttributeName value:[NSColor darkGrayColor] range:NSMakeRange(0, [str length])];  
+    [str addAttribute:NSForegroundColorAttributeName value:detailsColor range:NSMakeRange(0, [str length])];  
     [str addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]] range:NSMakeRange(0, [str length])];
     [att appendAttributedString:str];
   }  
