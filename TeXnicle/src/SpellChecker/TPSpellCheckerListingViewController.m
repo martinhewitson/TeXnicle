de91da84b2404434edeab93bc2f4f935d7ffbbe2
Martin Hewitson <martin@bobsoft-mac.de>
2012-07-13 08:16:05 +0200

    Added license text. Renamed variable.

--- TeXnicle/src/SpellChecker/TPSpellCheckerListingViewController.m
+++ TeXnicle/src/SpellChecker/TPSpellCheckerListingViewController.m
@@ -5,6 +5,25 @@
 //  Created by Martin Hewitson on 07/07/12.
 //  Copyright (c) 2012 bobsoft. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL DAN WOOD, MIKE ABDULLAH OR KARELIA SOFTWARE BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 #import "TPSpellCheckerListingViewController.h"
 #import "ProjectEntity.h"
@@ -483,8 +502,10 @@
   NSMutableArray *words = [NSMutableArray array];
   NSRange range = NSMakeRange(0, 0);
   NSRange lastRange = NSMakeRange(0, 0);
-  NSInteger wordCount = 0;
-  while (range.location < [aString length] && wordCount < 1000) {
+  
+  NSInteger misspelledWordCount = 0;
+  
+  while (range.location < [aString length] && misspelledWordCount <= 1000) {
     
     range = [checker checkSpellingOfString:aString startingAt:range.location];
     
@@ -504,7 +525,7 @@
       NSArray *corrections = [checker guessesForWordRange:range inString:aString language:nil inSpellDocumentWithTag:0];
       TPMisspelledWord *word = [TPMisspelledWord wordWithWord:misspelledWord corrections:corrections range:range parent:aFile];
       [words addObject:word];
-      wordCount++;
+      misspelledWordCount++;
     }
     
     // store last range
