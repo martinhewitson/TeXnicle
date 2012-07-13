fa62dbf5efdac7ff42fd3d3529b44688e8e855de
Martin Hewitson <martin@bobsoft-mac.de>
2012-07-13 08:16:35 +0200

    Added license text. Refactored string display methods.

--- TeXnicle/src/SpellChecker/TPSpellCheckedFile.m
+++ TeXnicle/src/SpellChecker/TPSpellCheckedFile.m
@@ -5,6 +5,25 @@
 //  Created by Martin Hewitson on 07/07/12.
 //  Copyright (c) 2012 bobsoft. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL DAN WOOD, MIKE ABDULLAH OR KARELIA SOFTWARE BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 #import "TPSpellCheckedFile.h"
 
@@ -45,49 +64,15 @@
 
 - (NSAttributedString*)selectedDisplayString
 {
-  
-  NSMutableParagraphStyle *ps = [[[NSMutableParagraphStyle alloc] init] autorelease];
-  [ps setParagraphStyle:[NSParagraphStyle defaultParagraphStyle]];
-  [ps setLineBreakMode:NSLineBreakByTruncatingTail];  
-  
-  NSString *text = nil;
-  if ([file isKindOfClass:[FileEntity class]]) {
-    text = [self.file name];
-  } else {
-    text = [self.file lastPathComponent];
-  }
-  
-  if ([text length]==0) {
-    text = @"<blank>";
-  }
-  if ([text length]>50) {
-    text = [[text substringToIndex:50] stringByAppendingString:@"..."];
-  }
-  
-  NSMutableAttributedString *att = [[[NSMutableAttributedString alloc] initWithString:text] autorelease]; 
-  
-  NSString *wordCountString = [NSString stringWithFormat:@" [%d] ", [self.words count]];
-  NSMutableAttributedString *str = [[[NSMutableAttributedString alloc] initWithString:wordCountString] autorelease];
-  [str addAttribute:NSForegroundColorAttributeName value:[NSColor redColor] range:NSMakeRange(0, [str length])];  
-  [str addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]] range:NSMakeRange(0, [str length])];
-  [att appendAttributedString:str];
-  
-  NSDateFormatter *formatter = [[[NSDateFormatter alloc] init] autorelease];
-  [formatter setDateStyle:NSDateFormatterNoStyle];
-  [formatter setTimeStyle:NSDateFormatterShortStyle];  
-  NSString *updateString = [NSString stringWithFormat:@"(updated: %@)", [formatter stringFromDate:self.lastCheck]];
-  str = [[[NSMutableAttributedString alloc] initWithString:updateString] autorelease];
-  [str addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]] range:NSMakeRange(0, [str length])];
-  [str addAttribute:NSForegroundColorAttributeName value:[NSColor whiteColor] range:NSMakeRange(0, [str length])];
-  [att appendAttributedString:str];
-  
-  // apply paragraph
-  [att addAttribute:NSParagraphStyleAttributeName value:ps range:NSMakeRange(0, [att length])];
-  
-  return att;
+  return [self stringForDisplayWithColor:[NSColor alternateSelectedControlTextColor] detailsColor:[NSColor alternateSelectedControlTextColor]];
 }
 
 - (NSAttributedString*)displayString
+{
+  return [self stringForDisplayWithColor:[NSColor redColor] detailsColor:[NSColor lightGrayColor]];
+}
+
+- (NSAttributedString*)stringForDisplayWithColor:(NSColor*)color detailsColor:(NSColor*)detailsColor
 {
   
   NSMutableParagraphStyle *ps = [[[NSMutableParagraphStyle alloc] init] autorelease];
@@ -101,18 +86,16 @@
     text = [self.file lastPathComponent];
   }
   
-  if ([text length]==0) {
-    text = @"<blank>";
-  }
-  if ([text length]>50) {
-    text = [[text substringToIndex:50] stringByAppendingString:@"..."];
-  }
-  
   NSMutableAttributedString *att = [[[NSMutableAttributedString alloc] initWithString:text] autorelease]; 
   
-  NSString *wordCountString = [NSString stringWithFormat:@" [%d] ", [self.words count]];
+  NSString *wordCountString = nil; 
+  if ([self.words count] >= 1000) {
+    wordCountString = [NSString stringWithFormat:@" [>%d] ", [self.words count]];
+  } else {
+    wordCountString = [NSString stringWithFormat:@" [%d] ", [self.words count]];
+  }
   NSMutableAttributedString *str = [[[NSMutableAttributedString alloc] initWithString:wordCountString] autorelease];
-  [str addAttribute:NSForegroundColorAttributeName value:[NSColor redColor] range:NSMakeRange(0, [str length])];  
+  [str addAttribute:NSForegroundColorAttributeName value:color range:NSMakeRange(0, [str length])];  
   [str addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]] range:NSMakeRange(0, [str length])];
   [att appendAttributedString:str];
   
@@ -122,7 +105,7 @@
   NSString *updateString = [NSString stringWithFormat:@"(updated: %@)", [formatter stringFromDate:self.lastCheck]];
   str = [[[NSMutableAttributedString alloc] initWithString:updateString] autorelease];
   [str addAttribute:NSFontAttributeName value:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]] range:NSMakeRange(0, [str length])];
-  [str addAttribute:NSForegroundColorAttributeName value:[NSColor lightGrayColor] range:NSMakeRange(0, [str length])];
+  [str addAttribute:NSForegroundColorAttributeName value:detailsColor range:NSMakeRange(0, [str length])];
   [att appendAttributedString:str];
   
   // apply paragraph
