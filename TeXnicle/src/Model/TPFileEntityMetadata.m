3e83773ee08aea7f94dac7a62b7887ee5f199f85
Martin Hewitson <martin@bobsoft-mac.de>
2012-07-16 10:29:15 +0200

    assign value to new notification.

--- TeXnicle/src/Model/TPFileEntityMetadata.m
+++ TeXnicle/src/Model/TPFileEntityMetadata.m
@@ -37,6 +37,7 @@
 #import "RegexKitLite.h"
 
 NSString * const TPFileMetadataSectionsUpdatedNotification = @"TPFileMetadataSectionsUpdatedNotification";
+NSString * const TPFileMetadataUpdatedNotification = @"TPFileMetadataUpdatedNotification";
 
 @implementation TPFileEntityMetadata
 
