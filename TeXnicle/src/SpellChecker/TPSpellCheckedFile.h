695aeb98617e0492fd2f4a796a313002c5190991
Martin Hewitson <martin@bobsoft-mac.de>
2012-07-16 13:16:07 +0200

    Retain the file because we need to keep it around.

--- TeXnicle/src/SpellChecker/TPSpellCheckedFile.h
+++ TeXnicle/src/SpellChecker/TPSpellCheckedFile.h
@@ -36,7 +36,7 @@
   BOOL needsUpdate;
 }
 
-@property (assign) id file;
+@property (retain) id file;
 @property (retain) NSDate *lastCheck;
 @property (retain) NSArray *words;
 @property (assign) BOOL needsUpdate;
