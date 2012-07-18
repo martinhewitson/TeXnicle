3877a9408e6d13571a9342fb685375214b8d28c3
Martin Hewitson <martin@bobsoft-mac.de>
2012-07-18 10:34:11 +0200

    make a nicer selection style which doesn't depend on having the focus.

--- TeXnicle/TeXEditor/Completion Window/TPPopupTableView.m
+++ TeXnicle/TeXEditor/Completion Window/TPPopupTableView.m
@@ -29,6 +29,46 @@
 
 @implementation TPPopupTableView
 
+- (void)highlightSelectionInClipRect:(NSRect)theClipRect
+{
+  
+  // this method is asking us to draw the hightlights for
+  // all of the selected rows that are visible inside theClipRect
+  
+  // 1. get the range of row indexes that are currently visible
+  // 2. get a list of selected rows
+  // 3. iterate over the visible rows and if their index is selected
+  // 4. draw our custom highlight in the rect of that row.
+  
+  NSRange         aVisibleRowIndexes = [self rowsInRect:theClipRect];
+  NSIndexSet *    aSelectedRowIndexes = [self selectedRowIndexes];
+  NSInteger       aRow = aVisibleRowIndexes.location;
+  NSInteger       anEndRow = aRow + aVisibleRowIndexes.length;
+  NSGradient *    gradient;
+  NSColor *       pathColor;
+  
+  // if the view is focused, use highlight color, otherwise use the out-of-focus highlight color
+  gradient = [[[NSGradient alloc] initWithColorsAndLocations:
+               [NSColor colorWithDeviceRed:(float)62/255 green:(float)133/255 blue:(float)197/255 alpha:1.0], 0.0,
+               [NSColor colorWithDeviceRed:(float)48/255 green:(float)95/255 blue:(float)152/255 alpha:1.0], 1.0, nil] retain]; //160 80
+  
+  pathColor = [[NSColor colorWithDeviceRed:(float)48/255 green:(float)95/255 blue:(float)152/255 alpha:1.0] retain];
+  
+  // draw highlight for the visible, selected rows
+  for (aRow; aRow < anEndRow; aRow++)
+  {
+    if([aSelectedRowIndexes containsIndex:aRow])
+    {
+      NSRect aRowRect = NSInsetRect([self rectOfRow:aRow], 1, 2); //first is horizontal, second is vertical
+      NSBezierPath * path = [NSBezierPath bezierPathWithRoundedRect:aRowRect xRadius:2.0 yRadius:2.0]; //6.0
+      [path setLineWidth: 2];
+      [pathColor set];
+      [path stroke];
+      
+      [gradient drawInBezierPath:path angle:90];
+    }
+  }
+}
 
 - (IBAction)moveDown:(id)sender
 {
