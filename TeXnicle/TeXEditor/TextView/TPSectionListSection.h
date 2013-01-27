41e8fa6acb827ac8ee5ce37f65c577822bb17755
Martin Hewitson <martin.hewitson@aei.mpg.de>
2013-01-27 14:14:47 +0100

    A new class to hold a section definition for the section list popup.

---
+++ TeXnicle/TeXEditor/TextView/TPSectionListSection.h
@@ -0,0 +1,13 @@
+//
+//  TPSectionListSection.h
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 27/01/13.
+//  Copyright (c) 2013 bobsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TPSectionListSection : NSObject
+
+@end
---
+++ TeXnicle/TeXEditor/TextView/TPSectionListSection.m
@@ -0,0 +1,13 @@
+//
+//  TPSectionListSection.m
+//  TeXnicle
+//
+//  Created by Martin Hewitson on 27/01/13.
+//  Copyright (c) 2013 bobsoft. All rights reserved.
+//
+
+#import "TPSectionListSection.h"
+
+@implementation TPSectionListSection
+
+@end
