9915db81707720b44fecdd546498d3bb8f92bb6a
Martin Hewitson <hewitson@aei.mpg.de>
2016-04-21 20:53:55 +0200

    update

--- TeXnicle/src/StandaloneEditor/MHSlideViewController.h
+++ TeXnicle/src/StandaloneEditor/MHSlideViewController.h
@@ -26,7 +26,7 @@
 //  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
 
 @interface MHSlideViewController : NSObject {
 @private
